cc73668a7f73a37ec37a4d04d7bb0ae3842afd34
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-10-04 11:14:42 +0530

    Update progress_bar.py

--- renamer/tools/progress_bar.py
+++ renamer/tools/progress_bar.py
@@ -25,13 +25,13 @@ async def progress_bar(current, total, status_msg, start, msg):
         current_message = f"""**{status_msg}** {round(percentage, 2)}%
 {progressbar}
 
-{HOLLOW_RED_CIRCLE} **🚀Speed**: {humanbytes(speed)}/s
+**🚀Speed**: {humanbytes(speed)}/s
 
-{HOLLOW_RED_CIRCLE} **Done✅**: {humanbytes(current)}
+**✅Done**: {humanbytes(current)}
 
-{HOLLOW_RED_CIRCLE} **📁Size**: {humanbytes(total)}
+**📁Size**: {humanbytes(total)}
 
-{HOLLOW_RED_CIRCLE} **⌚️Time Left**: {time_to_complete}"""
+**⌚️Time Left**: {time_to_complete}"""
         try:
             await msg.edit(text=current_message)
         except:
